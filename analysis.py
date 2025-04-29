0074b89303d8b60747b2d7630d4b630560620f5e
ahmadrgul <ahmadrg91@gmail.com>
2025-04-29 22:33:59 +0500

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import time
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -11,7 +12,7 @@ from selenium.webdriver.support import expected_conditions as EC
 def resource_path(relative_path):
     return os.path.join(getattr(sys, '_MEIPASS', os.path.abspath(".")), relative_path)
 
-highest_analysis_number = int(input("Latest Analysis Number: "))
+analysis_number = int(input("Latest Analysis Number: "))
 number_of_analysis = int(input("Number of Analysis to Download: "))
 
 download_path = os.path.join(os.environ["USERPROFILE"], "Downloads", "analysis")
@@ -27,15 +28,23 @@ options.add_experimental_option("prefs", {
 
 driver = webdriver.Chrome(service=Service(driver_path), options=options)
 wait = WebDriverWait(driver, 20)
+number_of_downloads = 0
 
-for n in range(highest_analysis_number, highest_analysis_number - number_of_analysis, -1):
-    driver.get(f'https://sandbox.pikker.ee/analysis/{n}/export/')
-    try:
-        button = wait.until(EC.presence_of_element_located((By.XPATH, '//button[@type="submit" and contains(@class, "btn") and contains(@class, "btn-primary")]')))
-        wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit" and contains(@class, "btn") and contains(@class, "btn-primary")]')))
-        button.click()
-    except:
+while number_of_downloads < number_of_analysis:
+    url = f'https://sandbox.pikker.ee/analysis/{analysis_number}/export/'
+    response = requests.get(url)
+    if response.status_code == 404:
+        analysis_number -= 1
         pass
-    time.sleep(10)
+    else:
+        driver.get(url)
+        try:
+            button = wait.until(EC.presence_of_element_located((By.XPATH, '//button[@type="submit" and contains(@class, "btn") and contains(@class, "btn-primary")]')))
+            wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit" and contains(@class, "btn") and contains(@class, "btn-primary")]')))
+            button.click()
+        except:
+            pass
+        time.sleep(5)
+        number_of_downloads += 1
 
 driver.quit()
